49a24b572792020d9de849bcad4f02401ea30deb
leungloh <18971461997@sina.cn>
2021-06-03 00:21:59 +0800

    fix bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="about">
-    <v-header></v-header>
-    <v-sidebar></v-sidebar>
+    <v-header />
+    <v-sidebar />
     <div class="content-box" :class="{ 'content-collapse': collapse }">
-      <v-tags></v-tags>
+      <v-tags />
       <div class="content">
-        <router-view v-slot="{ Component }">
           <transition name="move" mode="out-in">
             <keep-alive :include="tagsList">
-              <component :is="Component" />
+              <router-view />
             </keep-alive>
           </transition>
-        </router-view>
       </div>
     </div>
   </div>
