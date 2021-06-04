ad6527b67d8d50164a98378691019b73eea387e6
LeungLoh <843314152@qq.com>
2021-06-04 15:46:01 +0800

    go on

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
 import './assets/css/icon.css'
+// import * as echarts from 'echarts';
 const app = createApp(App)
 installElementPlus(app)
+// app.config.globalProperties.echarts = echarts;
 app
     .use(store)
     .use(router)
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -73,15 +73,48 @@
         </el-card>
       </el-col>
     </el-row>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <el-card shadow="hover">
+          <div class="echart-content">
+            <div id="bar1" :style="{ width: '100%', height: '100%' }"></div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="12">
+        <el-card shadow="hover">
+          <div class="echart-content">
+            <div id="bar2" :style="{ width: '100%', height: '100%' }"></div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <el-card shadow="hover">
+          <div class="echart-content">
+            <div id="bar3" :style="{ width: '100%', height: '100%' }"></div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="12">
+        <el-card shadow="hover">
+          <div class="echart-content">
+            <div id="bar4" :style="{ width: '100%', height: '100%' }"></div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
+import * as echarts from "echarts";
+import { onMounted, onUnmounted } from "vue";
 export default {
   data() {
     return {
       name: localStorage.getItem("ms_username"),
-
       statistics: [
         {
           name: "用户访问量",
@@ -103,34 +136,15 @@ export default {
         },
       ],
       todoList: [
-        {
-          title: "今天要修复100个bug",
-          status: false,
-        },
-        {
-          title: "今天要修复100个bug",
-          status: false,
-        },
-        {
-          title: "今天要写100行代码加几个bug吧",
-          status: false,
-        },
-        {
-          title: "今天要修复100个bug",
-          status: false,
-        },
-        {
-          title: "今天要修复100个bug",
-          status: true,
-        },
-        {
-          title: "今天要写100行代码加几个bug吧",
-          status: true,
-        },
+        { title: "今天要修复100个bug", status: false },
+        { title: "今天要修复100个bug", status: false },
+        { title: "今天要写100行代码加几个bug吧", status: false },
+        { title: "今天要修复100个bug", status: false },
+        { title: "今天要修复100个bug", status: true },
+        { title: "今天要写100行代码加几个bug吧", status: true },
       ],
     };
   },
-
   computed: {
     role() {
       return this.name == "admin" ? "超级管理员" : "普通用户";
@@ -138,6 +152,295 @@ export default {
     span() {
       return Math.floor(24 / this.statistics.length);
     },
+  },
+  setup() {
+    /// 声明定义一下echart
+    let echart = echarts;
+    onMounted(() => {
+      initChart();
+    });
+    onUnmounted(() => {
+      echart.dispose;
+    });
+    function initChart() {
+      let chart1 = echart.init(document.getElementById("bar1"));
+      let chart2 = echart.init(document.getElementById("bar2"));
+      let chart3 = echart.init(document.getElementById("bar3"));
+      let chart4 = echart.init(document.getElementById("bar4"));
+      let option = {
+        title: {
+          text: "柱状图",
+          left: "right",
+        },
+        tooltip: {
+          trigger: "axis",
+        },
+        grid: {
+          left: "6%",
+          right: "6%",
+          bottom: "6%",
+          containLabel: true,
+        },
+        legend: {
+          data: ["家电", "百货", "食品"],
+          left: "6%",
+          top: "top",
+          itemWidth: 15, //图例图标的宽
+          itemHeight: 15, //图例图标的高
+          textStyle: {
+            color: "#3a6186",
+            fontSize: 20,
+          },
+        },
+        calculable: true,
+        xAxis: [
+          {
+            type: "category",
+            data: ["周一", "周二", "周三", "周四", "周五"],
+            splitLine: { show: false }, //去除网格分割线
+            axisTick: { show: false }, //刻度
+            axisLine: {
+              //坐标线
+              lineStyle: {
+                type: "solid",
+                color: "#e7e7e7", //轴线的颜色
+                width: "2", //坐标线的宽度
+              },
+            },
+            axisLabel: {
+              textStyle: { color: "#3a6186" }, //坐标值的具体的颜色
+            },
+          },
+        ],
+        yAxis: [
+          {
+            type: "value",
+            axisLine: { show: false }, //线
+            splitLine: { lineStyle: { color: ["#e7e7e7"] } }, //分割线的颜色
+            axisTick: { show: false }, //刻度
+            axisLabel: {
+              textStyle: { color: "#3a6186" }, //坐标值的具体的颜色
+            },
+          },
+        ],
+        series: [
+          {
+            name: "家电",
+            type: "bar",
+            barWidth: 20,
+            data: [120, 132, 101, 134, 90, 230, 210],
+            itemStyle: {
+              normal: {
+                label: {
+                  show: true, //柱子上显示值
+                  position: "top", //值在柱子上方显示
+                },
+              },
+            },
+          },
+          {
+            name: "百货",
+            type: "bar",
+            barWidth: 20,
+            data: [220, 182, 191, 234, 290, 330, 310],
+            itemStyle: {
+              normal: {
+                label: {
+                  show: true, //柱子上显示值
+                  position: "top", //值在柱子上方显示
+                },
+              },
+            },
+          },
+          {
+            name: "食品",
+            type: "bar",
+            barWidth: 20,
+            data: [150, 232, 201, 154, 190, 330, 410],
+            itemStyle: {
+              normal: {
+                label: {
+                  show: true, //柱子上显示值
+                  position: "top", //值在柱子上方显示
+                },
+              },
+            },
+          },
+        ],
+      };
+      let option2 = {
+        title: {
+          text: "折线图",
+          left: "right",
+        },
+        tooltip: {
+          trigger: "axis",
+        },
+        grid: {
+          left: "6%",
+          right: "6%",
+          bottom: "6%",
+          containLabel: true,
+        },
+        legend: {
+          data: ["家电", "百货", "食品"],
+          left: "6%",
+          top: "top",
+          itemWidth: 15, //图例图标的宽
+          itemHeight: 15, //图例图标的高
+          textStyle: {
+            color: "#3a6186",
+            fontSize: 20,
+          },
+        },
+        calculable: true,
+        xAxis: {
+          type: "category",
+          data: ["六月", "七月", "八月", "九月", "十月"],
+          axisTick: { show: false }, //刻度
+          axisLabel: {
+            textStyle: { color: "#3a6186" }, //坐标值的具体的颜色
+          },
+        },
+        yAxis: {
+          type: "value",
+          splitLine: { lineStyle: { color: ["#e7e7e7"] } }, //分割线的颜色
+          axisTick: { show: false }, //刻度
+          axisLabel: {
+            textStyle: { color: "#3a6186" }, //坐标值的具体的颜色
+          },
+        },
+        series: [
+          {
+            name: "家电",
+            type: "line",
+            data: [120, 132, 101, 134, 90, 230, 210],
+          },
+          {
+            name: "百货",
+            type: "line",
+            data: [220, 182, 191, 234, 290, 330, 310],
+          },
+          {
+            name: "食品",
+            type: "line",
+            data: [150, 232, 201, 154, 190, 330, 410],
+          },
+        ],
+      };
+      let option3 = {
+        title: {
+          text: "饼状图",
+          left: "right",
+        },
+        tooltip: {
+          trigger: "axis",
+        },
+        grid: {
+          left: "6%",
+          right: "6%",
+          bottom: "6%",
+          containLabel: true,
+        },
+        legend: {
+          data: ["家电", "百货", "食品"],
+          left: "6%",
+          top: "top",
+          itemWidth: 15, //图例图标的宽
+          itemHeight: 15, //图例图标的高
+          textStyle: {
+            color: "#3a6186",
+            fontSize: 20,
+          },
+        },
+        calculable: true,
+        series: {
+          name: "demo",
+          type: "pie",
+          radius: "50%",
+          data: [
+            { name: "家电", value: "120" },
+            { name: "百货", value: "220" },
+            { name: "食品", value: "150" },
+          ],
+        },
+      };
+      let option4 = {
+        title: {
+          text: "曲线图",
+          left: "right",
+        },
+        tooltip: {
+          trigger: "axis",
+        },
+        grid: {
+          left: "6%",
+          right: "6%",
+          bottom: "6%",
+          containLabel: true,
+        },
+        legend: {
+          data: ["家电", "百货", "食品"],
+          left: "6%",
+          top: "top",
+          itemWidth: 15, //图例图标的宽
+          itemHeight: 15, //图例图标的高
+          textStyle: {
+            color: "#3a6186",
+            fontSize: 20,
+          },
+        },
+        calculable: true,
+        xAxis: {
+          type: "category",
+          data: ["六月", "七月", "八月", "九月", "十月"],
+          axisTick: { show: false }, //刻度
+          axisLabel: {
+            textStyle: { color: "#3a6186" }, //坐标值的具体的颜色
+          },
+        },
+        yAxis: {
+          type: "value",
+          splitLine: { lineStyle: { color: ["#e7e7e7"] } }, //分割线的颜色
+          axisTick: { show: false }, //刻度
+          axisLabel: {
+            textStyle: { color: "#3a6186" }, //坐标值的具体的颜色
+          },
+        },
+        series: [
+          {
+            name: "家电",
+            type: "line",
+            data: [120, 132, 101, 134, 90, 230, 210],
+            smooth: true,
+          },
+          {
+            name: "百货",
+            type: "line",
+            data: [220, 182, 191, 234, 290, 330, 310],
+            smooth: true,
+          },
+          {
+            name: "食品",
+            type: "line",
+            data: [150, 232, 201, 154, 190, 330, 410],
+            smooth: true,
+          },
+        ],
+      };
+      //防止越界，重绘canvas
+      window.onresize = function () {
+        chart1.resize();
+        chart2.resize();
+        chart3.resize();
+        chart4.resize();
+      };
+      chart1.setOption(option); //设置option
+      chart2.setOption(option2); //设置option
+      chart3.setOption(option3); //设置option
+      chart4.setOption(option4); //设置option
+    }
+    return { initChart };
   },
 };
 </script>
@@ -233,5 +536,14 @@ export default {
 .todo-item-del {
   text-decoration: line-through;
   color: #999;
+}
+.echart-content {
+  height: 500px;
+  display: flex;
+  justify-content: center;
+}
+.echart-content #bar1,
+#bar2 {
+  margin-top: 35px;
 }
 </style>
