be2c759ce4d79d3bd9dcd9ceb1187de33efba91d
LeungLoh <843314152@qq.com>
2021-06-11 11:44:55 +0800

    add cookie set

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,8 @@ const service = axios.create({
     // process.env.NODE_ENV === 'development' 来判断是否开发环境
     // easy-mock服务挂了，暂时不使用了
     // baseURL: 'https://www.easy-mock.com/mock/592501a391470c0ac1fab128',
-    timeout: 5000
+    timeout: 5000,
+    withCredentials: true
 });
 
 service.interceptors.request.use(
